2a7ffd991671f6f597bfbd1d8359e24966232935
Bonnie Owens <arlolocos@gmail.com>
2026-05-13 01:09:49 -0400

    Redirect pdf on mobile

--- src/app/sponsorships/page.tsx
+++ src/app/sponsorships/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react"
 
 export default function Sponsorships() {
   useEffect(() => {
-    if (window.innerWidth < 768) {
+    if (window.innerWidth < 640) {
       window.location.href = "/sponsorships.pdf"
     }
   }, [])
